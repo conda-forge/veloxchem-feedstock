125c076b207a95ce2095b3dcc2a8436b7a8e7c14
Roberto Di Remigio Eikås <robertodr@users.noreply.github.com>
2026-04-12 21:26:17 +0200

    feat: sync with latest master (#26)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,6 @@ cmake ^
     -DCMAKE_CXX_COMPILER:STRING=clang-cl ^
     -DCMAKE_C_COMPILER:STRING=clang-cl ^
     -DENABLE_ARCH_FLAGS:BOOL=OFF ^
-    -DVLX_LA_VENDOR:STRING="Generic" ^
     -DPython_EXECUTABLE:STRING="%PYTHON%" ^
     -DPYMOD_INSTALL_FULLDIR:PATH="Lib\site-packages\veloxchem"
 if errorlevel 1 exit 1
@@ -22,11 +21,3 @@ if errorlevel 1 exit 1
 :: install!
 cmake --build build --config Release --target install
 if errorlevel 1 exit 1
-
-:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
-:: This will allow them to be run on environment activation.
-for %%F in (activate deactivate) DO (
-    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
-    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
-    copy %RECIPE_DIR%\%%F.ps1 %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.ps1
-)
