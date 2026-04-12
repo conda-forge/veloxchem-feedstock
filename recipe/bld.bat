ecbf5b4580353d570aa8f57dfc8af3d0b7d54178
Roberto Di Remigio Eikås <robertodr@users.noreply.github.com>
2026-04-12 13:08:21 +0200

    fix: remove windows (de)activation scripts
    We don't link directly against MKL anymore, so there's no need to set KMP_DUPLICATE_LIB_OK=TRUE

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,11 +21,3 @@ if errorlevel 1 exit 1
 :: install!
 cmake --build build --config Release --target install
 if errorlevel 1 exit 1
-
-:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
-:: This will allow them to be run on environment activation.
-for %%F in (activate deactivate) DO (
-    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
-    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
-    copy %RECIPE_DIR%\%%F.ps1 %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.ps1
-)
